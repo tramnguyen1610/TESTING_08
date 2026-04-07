3766e8c02355b15849b6cd382dc0e2d6dc386d90
PhunThanhh <phuongthanhs922@gmail.com>
2026-04-07 23:21:01 +0700

    Update TimetablePage.java

--- KTAuto_Sele_Java_Nhom08/src/main/java/pageobjects/TimetablePage.java
+++ KTAuto_Sele_Java_Nhom08/src/main/java/pageobjects/TimetablePage.java
@@ -1,4 +1,27 @@
 package pageobjects;
 
-public class TimetablePage {
+import common.Constant;
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import java.time.Duration;
+
+public class TimetablePage extends GeneralPage {
+
+    public void clickBookTicketLink(String departStation, String arriveStation) {
+        WebDriverWait wait = new WebDriverWait(Constant.WEBDRIVER, Duration.ofSeconds(10));
+
+        // XPath tìm link "book ticket" theo ga đi và ga đến
+        String xpath = "//td[text()='" + departStation + "']/following-sibling::td[text()='"
+                + arriveStation + "']/..//a[text()='book ticket']";
+
+        WebElement bookTicketLink = wait.until(ExpectedConditions.elementToBeClickable(By.xpath(xpath)));
+
+        // Cuộn đến element trước khi click
+        ((JavascriptExecutor) Constant.WEBDRIVER).executeScript("arguments[0].scrollIntoView(true);", bookTicketLink);
+
+        bookTicketLink.click();
+    }
 }
