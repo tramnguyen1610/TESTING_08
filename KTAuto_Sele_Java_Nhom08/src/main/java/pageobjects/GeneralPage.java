d814f2c6f6bb5d8d527dc17f72d3e83ef576bf89
PhunThanhh <phuongthanhs922@gmail.com>
2026-04-07 23:19:31 +0700

    Update GeneralPage.java

--- KTAuto_Sele_Java_Nhom08/src/main/java/pageobjects/GeneralPage.java
+++ KTAuto_Sele_Java_Nhom08/src/main/java/pageobjects/GeneralPage.java
@@ -1,13 +1,48 @@
 package pageobjects;
 
-public class GeneralPage {
-    // General information about pages (Login and Logout tabs)
+import common.Constant;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
 
-    public void clickLoginTab() {
-        // Implementation
+public class GeneralPage {
+
+    // Locators cho các menu/tab dùng chung
+    private final By tabLogin = By.xpath("//a[@href='/Account/Login.cshtml']");
+    private final By tabBookTicket = By.xpath("//a[@href='/Page/BookTicketPage.cshtml']");
+    private final By tabChangePassword = By.xpath("//a[@href='/Account/ChangePassword.cshtml']");
+    private final By lblAccount = By.xpath("//div[@class='account']/strong");
+
+    // Elements
+    protected WebElement getTabLogin() {
+        return Constant.WEBDRIVER.findElement(tabLogin);
     }
 
-    public void clickLogoutTab() {
-        // Implementation
+    protected WebElement getTabBookTicket() {
+        return Constant.WEBDRIVER.findElement(tabBookTicket);
+    }
+
+    protected WebElement getTabChangePassword() {
+        return Constant.WEBDRIVER.findElement(tabChangePassword);
+    }
+
+    protected WebElement getLblAccount() {
+        return Constant.WEBDRIVER.findElement(lblAccount);
+    }
+
+    // Methods thao tác
+    public void clickLoginTab() {
+        getTabLogin().click();
+    }
+
+    public void clickBookTicketTab() {
+        getTabBookTicket().click();
+    }
+
+    public void clickChangePasswordTab() {
+        getTabChangePassword().click();
+    }
+
+    public String getAccountName() {
+        return getLblAccount().getText();
     }
 }
